1f7eb431f09b2d7fb471acf2719295270ce2425f
Khai Hoang <42855058+iamkhaihoang@users.noreply.github.com>
2022-06-15 23:41:27 -0400

    Add files via upload

---
+++ src/main/java/project_app.java
@@ -0,0 +1,21 @@
+package com.example;
+public class project_app {
+    public static void main(String[] args)
+    {
+        System.out.print("Enter Firstname: ");
+        //reading a string from the user
+        String firstname = System.console().readLine();
+        //String firstname = "Khai";
+        System.out.print("Enter Lastname: ");
+        //reading a string from the user
+        String lastname = System.console().readLine();
+        //String lastname = "Hoang";
+        String strhello = "Welcome and Hello " +firstname + " " + lastname;
+        strhello = strhello + " to DEVOPS team.";
+        System.out.println(strhello);
+    }
+
+    public String getStatus() {
+        return "OK";
+    }
+}
