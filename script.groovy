c573ad3bee803d066dabb19746052342fae163e3
iamkhaihoang <khai.hoang@outlook.com>
2022-07-06 20:44:00 -0400

    Update script security

--- script.groovy
+++ script.groovy
@@ -1,9 +1,22 @@
+def scriptApproval = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+
+String[] signs = [
+    "method org.jenkinsci.plugins.workflow.steps.FlowInterruptedException getCauses",
+    "method org.jenkinsci.plugins.workflow.support.steps.input.Rejection getUser"
+    ]
+
+for( String sign : signs ) {
+    scriptApproval.approveSignature(sign)
+}
+
+scriptApproval.save()
+
 def buildJar() {
     echo "building the app.."
     sh 'mvn package'
 }
 
-def buildDockerImage() {
+def buildImage() {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t iamkhaihoang/demo-app:1.0 .'
